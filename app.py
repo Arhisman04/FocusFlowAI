7f8a99c7650b5c9b9de883e1aa11309ede9961b5
Archisman <giriarch1999@gmail.com>
2026-06-10 23:44:20 +0000

    FINAL

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from services.agent_router import route_agent
 from services.coach_engine import generate_coach_message
 from services.intelligence_layer import build_ai_brain
@@ -861,21 +862,7 @@ print(app.url_map)
 def timer():
 
     return render_template("timer.html")
-import os
-from urllib.parse import quote_plus
-from pymongo import MongoClient
-username = "archisman"
-password = quote_plus("rxFrYMQylJAqv22z")
 
-MONGO_URI = f"mongodb://{username}:{password}@ac-z3ralgu-shard-00-00.rfnktuo.mongodb.net:27017,ac-z3ralgu-shard-00-01.rfnktuo.mongodb.net:27017,ac-z3ralgu-shard-00-02.rfnktuo.mongodb.net:27017/?ssl=true&replicaSet=atlas-vaml3i-shard-0&authSource=admin&appName=FocusFlowCluster"
-
-client = MongoClient(MONGO_URI)
-db = client["FocusFlowAI"]
-users_collection = db["users"]
-sessions_collection = db["sessions"]
-plans_collection = db["plans"]
-tasks_collection = db["tasks"]
-memory_collection = db["memory"]
 # =========================
 # 🚀 RUN SERVER
 # =========================
--- database/mongodb.py
+++ database/mongodb.py
@@ -1,14 +1,17 @@
+from dotenv import load_dotenv
 import os
-from urllib.parse import quote_plus
 from pymongo import MongoClient
 
-username = "archisman"
-password = quote_plus("rxFrYMQylJAqv22z")
+load_dotenv()
 
-MONGO_URI = f"mongodb://{username}:{password}@ac-z3ralgu-shard-00-00.rfnktuo.mongodb.net:27017,ac-z3ralgu-shard-00-01.rfnktuo.mongodb.net:27017,ac-z3ralgu-shard-00-02.rfnktuo.mongodb.net:27017/?ssl=true&replicaSet=atlas-vaml3i-shard-0&authSource=admin&appName=FocusFlowCluster"
+MONGO_URI = os.getenv("MONGO_URI")
+
+print("Mongo URI Loaded:", MONGO_URI)
 
 client = MongoClient(MONGO_URI)
+
 db = client["FocusFlowAI"]
+
 users_collection = db["users"]
 sessions_collection = db["sessions"]
 plans_collection = db["plans"]
